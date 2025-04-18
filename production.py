85fbfdd2da0757a96f885e9941da24ff1c597d42
Marco <pinam@carleton.edu>
2025-04-17 20:13:22 -0500

    Added code for production.py and tests for production.py

--- production.py
+++ production.py
@@ -1,3 +1,18 @@
 '''
 A file for the production code
 '''
+import sys
+
+def reverse_word(word):
+    return word[::-1]
+
+def reverse_all_words(phrase):
+    return ' '.join(reverse_word(w) for w in phrase.split())
+
+def main():
+    if len(sys.argv) > 1:
+        phrase = ' '.join(sys.argv[1:])
+        print(reverse_all_words(phrase))
+        
+if __name__ == '__main__':
+    main()
